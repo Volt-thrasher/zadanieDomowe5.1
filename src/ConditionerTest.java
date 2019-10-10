ba524d0566f88bf94813ac1ff4ffe4282cb5b7eb
Volt <sonianadashkevich@gmail.com>
2019-10-10 20:44:17 +0200

    AC control program bean format

--- src/ConditionerTest.java
+++ src/ConditionerTest.java
@@ -4,16 +4,19 @@ public class ConditionerTest {
         Room room2 = new Room(9, 18);
         Room room3 = new Room(12, 17, true);
 
-        System.out.println(room1.temperature);
+        System.out.println(room1.getTemperature());
         room1.makeCooler();
-        System.out.println(room1.temperature);
+        System.out.println(room1.getTemperature());
 
-        System.out.println(room2.temperature);
+        System.out.println(room2.getTemperature());
         room2.makeCooler();
-        System.out.println(room2.temperature);
+        System.out.println(room2.getTemperature());
 
-        System.out.println(room3.temperature);
+        System.out.println(room3.getTemperature());
         room3.makeCooler();
-        System.out.println(room3.temperature);
+        System.out.println(room3.getTemperature());
+
+        System.out.println("Kwadratura pokoju: " + room1.getSize() + "m2, aktualna temperatura: " + room1.getTemperature()
+                + " 'C");
     }
 }
--- src/Room.java
+++ src/Room.java
@@ -1,13 +1,15 @@
-public class Room {
-    double size;
-    double temperature;
-    boolean conditioner;
+class Room {
+    private double size;
+    private double temperature;
+    private boolean conditioner;
 
     Room(double s, double temp, boolean ac) {
         size = s;
         temperature = temp;
         conditioner = ac;
     }
+
+    double minTemp = 16;
 
     Room(double s, double temp) {
         size = s;
@@ -16,11 +18,20 @@ public class Room {
     }
 
     boolean makeCooler() {
-        if (temperature >= 16 && conditioner == true) {
+        if (temperature >= minTemp && conditioner) {
             temperature--;
             return true;
         } else {
             return false;
         }
     }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
 }
