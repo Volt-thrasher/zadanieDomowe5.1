8f827b180e46426375931ab1057fa1fc43097a82
Volt <sonianadashkevich@gmail.com>
2019-10-11 13:46:14 +0200

    minTemp field private, constructor changed

--- src/Room.java
+++ src/Room.java
@@ -2,18 +2,16 @@ class Room {
     private double size;
     private double temperature;
     private boolean conditioner;
+    private double minTemp = 16;
 
     Room(double s, double temp, boolean ac) {
-        size = s;
-        temperature = temp;
+        this(s, temp);
         conditioner = ac;
     }
 
-    double minTemp = 16;
-
     Room(double s, double temp) {
-        size = s;
-        temperature = temp;
+        this.size = s;
+        this.temperature = temp;
         conditioner = false;
     }
 
